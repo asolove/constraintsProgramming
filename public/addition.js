f47c331aee958565937f634c167d92fa1a83230d
Adam Solove <asolove@gmail.com>
2015-12-13 20:38:56 -0500

    AnimalCounts gives its NumberInputs a way to tell it when their value changes. This causes the display to update correctly for each input. But there isn't yet any logic to keep the total in sync with the number of cats and dogs.

--- public/addition.js
+++ public/addition.js
@@ -1,25 +1,33 @@
 var NumberInput = React.createFactory(React.createClass({
+  handleChange: function(e){
+    this.props.onChange(this.props.name, e.target.value);
+  },
   render: function(){
     return React.DOM.div({},
-      React.DOM.input({type: "range", min: 0, max: 10, value: this.props.value}),
+      React.DOM.input({type: "range", min: 0, max: 10, value: this.props.value, onChange: this.handleChange}),
       React.DOM.span(null, this.props.value + " " + this.props.name)
     );
   }
 }));
 
 var AnimalCounts = React.createFactory(React.createClass({
-  getInitialState: function() {
+  getInitialState: function(){
     return {
       dogs: 2,
       cats: 3,
       total: 5
     };
   },
+  handleChange: function(name, value){
+    var stateChange = []
+    stateChange[name] = value;
+    this.setState(stateChange);
+  },
   render: function(){
     return React.DOM.div(null,
-      NumberInput({name: "dogs", value: this.state.dogs}),
-      NumberInput({name: "cats", value: this.state.cats}),
-      NumberInput({name: "total", value: this.state.total})
+      NumberInput({name: "dogs", value: this.state.dogs, onChange: this.handleChange}),
+      NumberInput({name: "cats", value: this.state.cats, onChange: this.handleChange}),
+      NumberInput({name: "total", value: this.state.total, onChange: this.handleChange})
     );
   }
 }))

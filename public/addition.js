8234f6d9bf41301ed62aae08ab94a5fa2d1367f6
Adam Solove <asolove@gmail.com>
2015-12-13 20:33:11 -0500

    Expand to showing a list of numeric inputs for several variables. Note that the number of each animal is owned by the AnimalCounts component and just passed down to the NumberInput. For NumberInput to change its value, it's going to have to tell AnimalCounts to do it.

--- public/addition.js
+++ public/addition.js
@@ -1,13 +1,29 @@
 var NumberInput = React.createFactory(React.createClass({
   render: function(){
     return React.DOM.div({},
-      React.DOM.span(null, this.props.name),
       React.DOM.input({type: "range", min: 0, max: 10, valie: this.props.value}),
-      React.DOM.span(null, this.props.value)
-      );
+      React.DOM.span(null, this.props.value + " " + this.props.name)
+    );
   }
 }));
 
+var AnimalCounts = React.createFactory(React.createClass({
+  getInitialState: function() {
+    return {
+      dogs: 2,
+      cats: 3,
+      total: 5
+    };
+  },
+  render: function(){
+    return React.DOM.div(null,
+      NumberInput({name: "dogs", value: this.state.dogs}),
+      NumberInput({name: "cats", value: this.state.cats}),
+      NumberInput({name: "total", value: this.state.total})
+    );
+  }
+}))
+
 ReactDOM.render(
-  NumberInput({name: "Things", value: 2}),
+  AnimalCounts(),
   document.getElementById("container"));
